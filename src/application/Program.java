0c1aa831ba066154653745ab82a6540ecd3f7e91
acmors <marcosantoniosilva047@gmail.com>
2024-07-19 21:11:40 -0300

    atualizacao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,30 @@
 package application;
 
-public class Program {
+import java.util.Locale;
+import java.util.Scanner;
 
+public class Program {
+	
+	public static void main(String args[]) {
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the number of employees: ");
+		int n = scan.nextInt();
+		
+		for(int i = 0; i<n; i++) {
+			System.out.println("Employee #" + i +":");
+			System.out.print("Outsourced (y/n)?");
+			String answer = scan.next();
+			
+			System.out.print("Name: ");
+			String name = scan.next();
+			System.out.print("Hour: ");
+			int hour = scan.nextInt();
+			System.out.print("Value per hour:");
+			double valuePerHour = scan.nextDouble();
+		}
+		
+		
+	}
 }
--- src/entities/OutSourceEmployee.java
+++ src/entities/OutSourceEmployee.java
@@ -1,5 +1,29 @@
 package entities;
 
-public class OutSourceEmployee {
+public class OutSourceEmployee extends Employee{
+		
+	private Double addicionalCharge;
 
+	public OutSourceEmployee() {
+		super();
+	}
+
+	public OutSourceEmployee(String name, Integer hours, Double valuePerHour, Double addicionalCharge) {
+		super(name, hours, valuePerHour);
+		this.addicionalCharge = addicionalCharge;
+	}
+
+	public Double getAddicionalCharge() {
+		return addicionalCharge;
+	}
+
+	public void setAddicionalCharge(Double addicionalCharge) {
+		this.addicionalCharge = addicionalCharge;
+	}
+	
+	@Override
+	public Double payment() {
+		
+		return super.payment() + addicionalCharge * 1.10;
+	}
 }
